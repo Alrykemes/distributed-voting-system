bd65bd1556c9b6103f2bf43604e471b35fbacd73
CaioCesarMDS <caiocesarsts@gmail.com>
2025-07-08 21:02:42 -0300

    feat: add header and theme-toogle components

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,9 @@
+import { Header } from "@/components/header";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-5xl text-red-500">Caio lindo</h1>
+    <div className="">
+        <Header/>
     </div>
   );
 }
--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,5 +1,14 @@
-export function Header() {
-    return (<header>
+import {Vote} from "lucide-react";
+import {ThemeToggle} from "@/components/theme-toggle";
 
-        </header>)
+export function Header() {
+    return (<header className="border-b">
+        <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+            <div className="flex items-center gap-2">
+                <Vote className="h-8 w-8 text-primary" />
+                <h1 className="text-2xl font-bold text-foreground">VoteHub</h1>
+            </div>
+            <ThemeToggle />
+        </div>
+    </header>)
 }
